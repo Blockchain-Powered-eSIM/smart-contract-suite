dd4232079a081282edea2e06db59182c35a71bcf
ManulParihar <manul.singh.parihar@gmail.com>
2024-05-11 11:32:55 +0530

    Added _init and init functions

--- contracts/device-wallet/DeviceWallet.sol
+++ contracts/device-wallet/DeviceWallet.sol
@@ -6,8 +6,12 @@ import { Initializable } from "@openzeppelin/contracts-upgradeable/proxy/utils/I
 import { BeaconProxy } from "@openzeppelin/contracts/proxy/beacon/BeaconProxy.sol";
 import { UUPSUpgradeable } from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import { OwnableUpgradeable } from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-import { UpgradeableBeacon } from "../proxy/UpgradeableBeacon.sol";
 
+// TODO: Add UpgradeableBeacon contract based on https://github.com/OpenZeppelin/openzeppelin-contracts/blob/0db76e98f90550f1ebbb3dea71c7d12d5c533b5c/contracts/proxy/UpgradeableBeacon.sol
+// import { UpgradeableBeacon } from "../proxy/UpgradeableBeacon.sol";
+
+
+// TODO: Add ReentrancyGuard
 contract DeviceWallet is Initializable, UUPSUpgradeable, OwnableUpgradeable {
 
     /// @notice String identifier to uniquely identify user's device
@@ -15,6 +19,9 @@ contract DeviceWallet is Initializable, UUPSUpgradeable, OwnableUpgradeable {
 
     /// @notice Mapping from eSIMUniqueIdentifier to the respective eSIM wallet contract
     mapping(string => address) public eSIMUniqueIdentifierToESIMWallet;
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() initializer {}
 
     /// @dev Internal function to initialise the DeviceWallet contract
     function _init(
--- contracts/esim-wallet/ESIMWallet.sol
+++ contracts/esim-wallet/ESIMWallet.sol
@@ -6,10 +6,32 @@ import { Initializable } from "@openzeppelin/contracts-upgradeable/proxy/utils/I
 import { BeaconProxy } from "@openzeppelin/contracts/proxy/beacon/BeaconProxy.sol";
 import { UUPSUpgradeable } from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import { OwnableUpgradeable } from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-import { UpgradeableBeacon } from "../proxy/UpgradeableBeacon.sol";
 
+
+// TODO: Add ReentrancyGuard
 contract ESIMWallet is Initializable, UUPSUpgradeable, OwnableUpgradeable {
 
     /// @notice String identifier to uniquely identify eSIM wallet
     string public eSIMUniqueIdentifier;
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() initializer {}
+
+    // TODO: create interfaces
+    /// @inheritdoc IESIMWallet
+    function init(
+        string calldata _eSIMUniqueIdentifier
+    ) external virtual override initializer {
+        _init(
+            _eSIMUniqueIdentifier
+        );
+    }
+
+    /// @dev Internal function to initialise the DeviceWallet contract
+    function _init(
+        string calldata _eSIMUniqueIdentifier
+    ) internal {
+        
+        eSIMUniqueIdentifier = _eSIMUniqueIdentifier;
+    }
 }
