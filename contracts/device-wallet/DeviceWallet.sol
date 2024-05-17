a649fcb9ebd0ca22c2c25b080f424524a2ce3aa8
ManulParihar <manul.singh.parihar@gmail.com>
2024-05-17 16:17:26 +0530

    Added interface, updated device wallet and esim wallet contracts

--- contracts/device-wallet/DeviceWallet.sol
+++ contracts/device-wallet/DeviceWallet.sol
@@ -2,37 +2,85 @@ pragma solidity ^0.8.18;
 
 // SPDX-License-Identifier: MIT
 
+import { Address } from "@openzeppelin/contracts/utils/Address.sol";
 import { Initializable } from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import { BeaconProxy } from "@openzeppelin/contracts/proxy/beacon/BeaconProxy.sol";
-import { UUPSUpgradeable } from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
-import { OwnableUpgradeable } from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+
+// TODO: Make the contract upgradable
+// import { BeaconProxy } from "@openzeppelin/contracts/proxy/beacon/BeaconProxy.sol";
+// import { UUPSUpgradeable } from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+// import { OwnableUpgradeable } from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
 // TODO: Add UpgradeableBeacon contract based on https://github.com/OpenZeppelin/openzeppelin-contracts/blob/0db76e98f90550f1ebbb3dea71c7d12d5c533b5c/contracts/proxy/UpgradeableBeacon.sol
 // import { UpgradeableBeacon } from "../proxy/UpgradeableBeacon.sol";
 
+error OnlyESIMWalletAdmin();
+error OnlyESIMWalletAdminOrDeviceWallet();
 
 // TODO: Add ReentrancyGuard
-contract DeviceWallet is Initializable, UUPSUpgradeable, OwnableUpgradeable {
+contract DeviceWallet is Initializable {
+
+    using Address for address;
+
+    /// @notice eSIM wallet project's admin wallet address
+    address public eSIMWalletAdmin;
 
     /// @notice String identifier to uniquely identify user's device
     string public deviceUniqueIdentifier;
 
-    /// @notice Mapping from eSIMUniqueIdentifier to the respective eSIM wallet contract
-    mapping(string => address) public eSIMUniqueIdentifierToESIMWallet;
+    /// @notice Mapping from eSIMUniqueIdentifier to the respective eSIM wallet address
+    mapping(string => address) public eSIMUniqueIdentifierToESIMWalletAddress;
+
+    modifier onlyESIMWalletAdmin() {
+        if(msg.sender != eSIMWalletAdmin) revert OnlyESIMWalletAdmin();
+        _;
+    }
+
+    modifier onlyESIMWalletAdminOrDeviceWallet() {
+        if(msg.sender != eSIMWalletAdmin || msg.sender != address(this)) revert OnlyESIMWalletAdminOrDeviceWallet();
+        _;
+    }
 
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() initializer {}
 
-    /// @dev Internal function to initialise the DeviceWallet contract
-    function _init(
-        string calldata _deviceUniqueIdentifier
+    /// TOOD: Create a mapping of all the device wallets in the device wallet factory to make sure 
+    /// only the device wallets deployed by the factory contract are recognised
+    /// Initialises the device wallet and deploys eSIM wallets for any already existing eSIMs
+    /// @param _deviceUniqueIdentifier String to uniquely identify the device wallet
+    /// @param _eSIMUniqueIdentifiers Unique identifiers for already existing eSIMs
+    function init(
+        address _eSIMWalletAdmin,
+        string calldata _deviceUniqueIdentifier,
         string[] calldata _eSIMUniqueIdentifiers
-    ) internal {
+    ) external returns (address) {
+        require(_eSIMWalletAdmin != address (0), "eSIM wallet admin cannot be zero address");
+        require(bytes(_deviceUniqueIdentifier).length != 0, "Device unique identifier cannot be zero");
         
+        eSIMWalletAdmin = _eSIMWalletAdmin;
         deviceUniqueIdentifier = _deviceUniqueIdentifier;
 
-        for(let i=0; i<_eSIMUniqueIdentifiers.length; ++i) {
-            // deploy eSIM Smart wallets
+        if(_eSIMUniqueIdentifiers.length != 0) {
+            for(uint256 i=0; i<_eSIMUniqueIdentifiers.length; ++i) {
+                address eSIMWalletAddress = eSIMUniqueIdentifierToESIMWalletAddress[_eSIMUniqueIdentifiers[i]];
+                require(eSIMWalletAddress == address(0), "eSIM wallet already exists");
+                
+                // TODO: deploy esim wallets
+
+                setESIMUniqueIdentifierForAnESIMWallet(eSIMWalletAddress, _eSIMUniqueIdentifiers[i]);
+            }
         }
+
+        return address(this);
+    }
+
+    function setESIMUniqueIdentifierForAnESIMWallet(
+        address _eSIMWalletAddress,
+        string calldata _eSIMUniqueIdentifier
+    ) onlyESIMWalletAdminOrDeviceWallet public {
+        // TODO: import esim wallet contract and call respective set identifier function
+    }
+
+    receive() external payable {
+        // receive ETH
     }
 }
--- contracts/esim-wallet/ESIMWallet.sol
+++ contracts/esim-wallet/ESIMWallet.sol
@@ -2,16 +2,27 @@ pragma solidity ^0.8.18;
 
 // SPDX-License-Identifier: MIT
 
-import { Initializable } from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { Initializable } from "@openzeppelin/contracts/proxy/utils/Initializable.sol";
+import { Address } from "@openzeppelin/contracts/utils/Address.sol";
+import { IOwnableESIMWallet } from "../interfaces/IOwnableESIMWallet.sol";
 // TODO: Add the below mentioned  imports as per need
 // import { BeaconProxy } from "@openzeppelin/contracts/proxy/beacon/BeaconProxy.sol";
 // import { UUPSUpgradeable } from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 // import { OwnableUpgradeable } from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
+error OnlyDeviceWallet();
 
 // TODO: Add ReentrancyGuard
-contract ESIMWallet is Initializable {
+contract ESIMWallet is IOwnableESIMWallet, Ownable, Initializable {
+
+    using Address for address;
+
+    /// Emitted when the eSIM wallet is deployed
+    event ESIMWalletDeployed(address indexed _eSIMWalletAddress, address indexed _deviceWalletAddress, address indexed _owner);
+
+    /// @notice Emitted when the eSIM unique identifier is initialised
+    event ESIMUniqueIdentifierInitialised(string _eSIMUniqueIdentifier);
 
     /// @notice Address of the eSIM wallet factory contract
     address public eSIMWalletFactory;
@@ -19,28 +30,69 @@ contract ESIMWallet is Initializable {
     /// @notice String identifier to uniquely identify eSIM wallet
     string public eSIMUniqueIdentifier;
 
+    /// @notice Address of the device wallet associated with this eSIM wallet
+    address public deviceWalletAddress;
+
+    modifier onlyDeviceWallet() {
+        if(msg.sender != deviceWalletAddress) revert OnlyDeviceWallet();
+        _;
+    }
+
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() initializer {}
 
     // TODO: create interfaces
     function init(
-        string calldata _eSIMUniqueIdentifier,
-        address _eSIMWalletFactory
-    ) external virtual initializer {
-        _init(
-            _eSIMUniqueIdentifier,
-            _eSIMWalletFactory
-        );
+        address _eSIMWalletFactoryAddress,
+        address _deviceWalletAddress,
+        address _owner
+    ) external override initializer {
+        require(_owner != address(0), "Owner cannot be address zero");
+        require(_eSIMWalletFactoryAddress != address(0), "eSIM wallet factory address cannot be zero");
+        require(_deviceWalletAddress != address(0), "Device wallet address cannot be zero");
+
+        eSIMWalletFactory = _eSIMWalletFactoryAddress;
+        deviceWalletAddress = _deviceWalletAddress;
+
+        _transferOwnership(_owner);
+
+        emit ESIMWalletDeployed(address(this), _deviceWalletAddress, _owner);
     }
 
-    /// @dev Internal function to initialise the DeviceWallet contract
-    function _init(
-        string calldata _eSIMUniqueIdentifier,
-        address _eSIMWalletFactory
-    ) internal {
-        eSIMWalletFactory = _eSIMWalletFactory;
+    /// @notice Since buying the eSIM (along with data bundle) happens before the identifier is generated,
+    ///         the identifier is to be set separately after the wallet is deployed and eSIM is created
+    /// @dev This function can only be called once
+    /// @param _eSIMUniqueIdentifier String that uniquely identifies eSIM wallet
+    function setESIMUniqueIdentifier(
+        string calldata _eSIMUniqueIdentifier
+    ) onlyDeviceWallet external {
+        require(bytes(eSIMUniqueIdentifier).length == 0, "eSIM unique identifier already initialised");
+        require(bytes(_eSIMUniqueIdentifier).length != 0, "eSIM unique identifier cannot be zero");
+
         eSIMUniqueIdentifier = _eSIMUniqueIdentifier;
 
-        return address(this);
+        emit ESIMUniqueIdentifierInitialised(_eSIMUniqueIdentifier);
+    }
+
+    function transferOwnership(address newOwner)
+        public
+        override(IOwnableESIMWallet, Ownable)
+    {
+        // Only the admin of deviceWalletFactory contract can transfer ownership
+        require(
+            isTransferApproved(owner(), msg.sender),
+            "OwnableSmartWallet: Transfer is not allowed"
+        ); // F: [OSW-4]
+
+        // Approval is revoked, in order to avoid unintended transfer allowance
+        // if this wallet ever returns to the previous owner
+        if (msg.sender != owner()) {
+            _setApproval(owner(), msg.sender, false); // F: [OSW-5]
+        }
+        _transferOwnership(newOwner); // F: [OSW-5]
+    }
+
+    receive() external payable {
+        // receive ETH
     }
 }
