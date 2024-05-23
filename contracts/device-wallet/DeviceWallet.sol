e07f582d950457835ab5d18b88c051910c8df2ea
ManulParihar <manul.singh.parihar@gmail.com>
2024-05-23 22:06:11 +0530

    Updated contract to fetch esim wallet admin from the factory. To avoid admin mismatch

--- contracts/device-wallet/DeviceWallet.sol
+++ contracts/device-wallet/DeviceWallet.sol
@@ -7,6 +7,7 @@ import { Address } from "@openzeppelin/contracts/utils/Address.sol";
 import { Initializable } from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import { ESIMWallet } from "../esim-wallet/ESIMWallet.sol";
 import { ESIMWalletFactory } from "../esim-wallet/ESIMWalletFactory.sol";
+import { DeviceWalletFactory } from "./DeviceWalletFactory.sol";
 
 // TODO: Make the contract upgradable
 // import { BeaconProxy } from "@openzeppelin/contracts/proxy/beacon/BeaconProxy.sol";
@@ -18,20 +19,21 @@ import { ESIMWalletFactory } from "../esim-wallet/ESIMWalletFactory.sol";
 
 error OnlyESIMWalletAdmin();
 error OnlyESIMWalletAdminOrDeviceWalletOwner();
+error OnlyESIMWalletAdminOrDeviceWalletFactory();
 
 // TODO: Add ReentrancyGuard
 contract DeviceWallet is Ownable, Initializable {
 
     using Address for address;
 
-    /// @notice eSIM wallet project's admin wallet address
-    address public eSIMWalletAdmin;
-
     /// @notice ETH balance of the contract
     uint256 public ethBalance;
 
     /// @notice ESIM wallet factory contract instance
     ESIMWalletFactory public eSIMWalletFactory;
+
+    /// @notice Device wallet factory contract instance
+    DeviceWalletFactory public deviceWalletFactory;
 
     /// @notice String identifier to uniquely identify user's device
     string public deviceUniqueIdentifier;
@@ -43,12 +45,23 @@ contract DeviceWallet is Ownable, Initializable {
     mapping(address => bool) public isValidESIMWallet;
 
     modifier onlyESIMWalletAdmin() {
-        if(msg.sender != eSIMWalletAdmin) revert OnlyESIMWalletAdmin();
+        if(msg.sender != deviceWalletFactory.eSIMWalletAdmin()) revert OnlyESIMWalletAdmin();
+        _;
+    }
+
+    modifier onlyESIMWalletAdminOrDeviceWalletFactory() {
+        if(
+            msg.sender != deviceWalletFactory.eSIMWalletAdmin() && 
+            msg.sender != address(deviceWalletFactory)
+        ) revert OnlyESIMWalletAdminOrDeviceWalletFactory();
         _;
     }
 
     modifier onlyESIMWalletAdminOrDeviceWalletOwner() {
-        if(msg.sender != eSIMWalletAdmin || msg.sender != owner) revert OnlyESIMWalletAdminOrDeviceWalletOwner();
+        if(
+            msg.sender != deviceWalletFactory.eSIMWalletAdmin() && 
+            msg.sender != owner
+        ) revert OnlyESIMWalletAdminOrDeviceWalletOwner();
         _;
     }
 
@@ -56,7 +69,7 @@ contract DeviceWallet is Ownable, Initializable {
     constructor() initializer {}
 
     /// @notice Initialises the device wallet and deploys eSIM wallets for any already existing eSIMs
-    /// @param _eSIMWalletAdmin Admin address of eSIM wallet project
+    /// @param _deviceWalletFactoryAddress Device wallet factory smart contract address
     /// @param _eSIMWalletFactoryAddress eSIM wallet factory smart contract address
     /// @param _owner User's address (Owner of device wallet and related eSIM wallet smart contracts)
     /// @param _deviceUniqueIdentifier String to uniquely identify the device wallet
@@ -64,7 +77,7 @@ contract DeviceWallet is Ownable, Initializable {
     /// @param _dataBundlePrices List of data bundle prices for the respective data bundle IDs
     /// @param _eSIMUniqueIdentifiers Unique identifiers for already existing eSIMs
     function init(
-        address _eSIMWalletAdmin,
+        address _deviceWalletFactoryAddress,
         address _eSIMWalletFactoryAddress,
         address _owner,
         string calldata _deviceUniqueIdentifier,
@@ -72,13 +85,14 @@ contract DeviceWallet is Ownable, Initializable {
         uint256[] _dataBundlePrices,
         string[] calldata _eSIMUniqueIdentifiers
     ) external payable returns (address) {
+        require(_deviceWalletFactoryAddress != address(0), "Device wallet factory cannot be zero address");
         require(_eSIMWalletAdmin != address (0), "eSIM wallet admin cannot be zero address");
         require(_owner != address (0), "eSIM wallet owner cannot be zero address");
         require(bytes(_deviceUniqueIdentifier).length != 0, "Device unique identifier cannot be zero");
         require(_dataBundleIDs.length > 0, "Data bundle ID array cannot be zero");
         require(_dataBundleIDs.length == _dataBundlePrices.length, "Array mismatch");
         
-        eSIMWalletAdmin = _eSIMWalletAdmin;
+        deviceWalletFactory = DeviceWalletFactory(_deviceWalletFactoryAddress);
         deviceUniqueIdentifier = _deviceUniqueIdentifier;
         eSIMWalletFactory = ESIMWalletFactory(_eSIMWalletFactoryAddress);
 
