c7d3a63340a03084cd017764ce1554dac0f5b91e
ManulParihar <manul.singh.parihar@gmail.com>
2024-05-24 16:23:53 +0530

    Updated modifier to onlyESIMWalletAdmin in setESIMUniqueIdentifierForAnESIMWallet

--- contracts/device-wallet/DeviceWallet.sol
+++ contracts/device-wallet/DeviceWallet.sol
@@ -187,7 +187,7 @@ contract DeviceWallet is Ownable, Initializable {
     function setESIMUniqueIdentifierForAnESIMWallet(
         address _eSIMWalletAddress,
         string calldata _eSIMUniqueIdentifier
-    ) onlyESIMWalletAdminOrDeviceWalletOwner public returns(string calldata) {
+    ) onlyESIMWalletAdmin public returns(string calldata) {
         require(ESIMWalletFactory.isValidESIMWallet(_eSIMWalletAddress) == true, "Unknown eSIM wallet address provided");
         require(eSIMUniqueIdentifierToESIMWalletAddress[_eSIMUniqueIdentifier] == address(0), "eSIM unique identifier already set for the provided eSIM wallet");
         
