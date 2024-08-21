5a8a6e207881f3f16ddcbdfe4b4b62370770d478
ManulParihar <manul.singh.parihar@gmail.com>
2024-08-22 01:19:13 +0530

    Updated storage variables, introduced registry, added batch history function

--- contracts/LazyWalletRegistry.sol
+++ contracts/LazyWalletRegistry.sol
@@ -6,11 +6,16 @@ import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Ini
 import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
+import {Registry} from "./Registry.sol";
+
 /// @notice Contract for deploying the factory contracts and maintaining registry
 contract LazyWalletRegistry is Initializable, UUPSUpgradeable, OwnableUpgradeable, RegistryHelper {
 
     /// @notice Address (owned/controlled by eSIM wallet project) that can upgrade contracts
     address public upgradeManager;
+
+    /// @notice Registry contract instance
+    Registry public registry;
 
     /// @notice Data Bundle related details stored in the eSIM wallet
     struct DataBundleDetail {
@@ -20,12 +25,24 @@ contract LazyWalletRegistry is Initializable, UUPSUpgradeable, OwnableUpgradeabl
 
     /// @notice Details related to eSIM purchased by the fiat user
     struct eSIMDetails {
-        uint256 id;
         DataBundleDetail[] history;
     }
 
-    /// @notice Mapping from fiat user's unique device identifier to the eSIM details
-    mapping(string => eSIMDetails[]) public deviceIdentifierToESIMDetails;
+    /// @notice Struct to store list of all eSIMs associated with a device
+    struct associatedESIMIdentifiers {
+        string[] eSIMIdentifiers;
+    }
+
+    /// @notice Device identifier <> eSIM identifier <> eSIMDetails(purchase history)
+    mapping(string => mapping(string => eSIMDetails)) public deviceIdentifierToESIMDetails;
+
+    /// @notice Mapping from eSIM unique identifier to device unique identifier
+    /// @dev A device identifier can have multiple associated eSIM identifiers.
+    /// But an eSIM identifier can have only a single device identifier.
+    mapping(string => string) public eSIMIdentifierToDeviceIdentifier;
+
+    /// @notice Device identifier <> List of associated eSIM identifiers
+    mapping(string => associatedESIMIdentifiers) public eSIMIdentifiersAssociatedWithDeviceIdentifier;
 
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() initializer {
@@ -39,10 +56,38 @@ contract LazyWalletRegistry is Initializable, UUPSUpgradeable, OwnableUpgradeabl
     override
     {}
 
-    function initialize(address _upgradeManager) external initializer {
+    function initialize(
+        address _registry,
+        address _upgradeManager
+    ) external initializer {
+        require(_registry != address(0), "Registry contract address cannot be zero");
         require(_upgradeManager != address(0), "Upgrade Manager address cannot be zero address");
+        
+        registry = Registry(_registry);
         upgradeManager = _upgradeManager;
 
         __Ownable_init(_upgradeManager);
+    }
+
+    function batchPopulateHistory(
+        string[] calldata _deviceUniqueIdentifiers,
+        string[][] calldata _eSIMUniqueIdentifiers,
+        eSIMDetails[][] calldata _eSIMDetails
+    ) external {
+        uint256 len = _deviceUniqueIdentifiers.length;
+        require(len == _eSIMUniqueIdentifiers.length, "Unequal array provided");
+        require(len == _eSIMDetails.length, "Unequal array provided");
+
+        for(uint256 i=0; i<len; ++i) {
+            _populateHistory(_deviceUniqueIdentifiers[i], _eSIMUniqueIdentifiers[i], _eSIMDetails[i]);
+        }
+    }
+
+    function _populateHistory(
+        string calldata _deviceUniqueIdentifier,
+        string[] calldata _eSIMUniqueIdentifiers,
+        eSIMDetails[] calldata _eSIMDetails
+    ) internal {
+
     }
 }
